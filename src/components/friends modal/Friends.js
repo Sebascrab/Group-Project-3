dabea57f2f936baf0e098506de158bcc475a6b0b
Alexander <alexanders.tischner@outlook.com>
2022-11-23 13:08:01 -0700

    changes

--- src/components/friends modal/Friends.js
+++ src/components/friends modal/Friends.js
@@ -5,14 +5,19 @@ import avatarPic from "../../assets/images/Sebas-dog.jpeg"
 
 export const Friend = () => {
     return (
-        <Row className="friend-profile-item">
-            <Col xs lg="2">
-                <Avatar className="avatar" src={avatarPic}></Avatar>
-            </Col>
-            <Col className="friend-user" md="auto">
-                <strong><h3>Sebastian Tischner</h3></strong>
-                <h6>@sebascrab</h6>
-            </Col>
-        </Row>
+        <ul>
+            <li>
+                <Row className="friend-profile-item">
+                    <Col xs lg="2">
+                        <Avatar className="avatar" src={avatarPic}></Avatar>
+                    </Col>
+                    <Col className="friend-user" md="auto">
+                        <strong><h3>Sebastian Tischner</h3></strong>
+                        <h6>@sebascrab</h6>
+                    </Col>
+                </Row>
+            </li>
+        </ul>
+
     )
 }
