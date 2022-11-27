e5cdb50bda1a0e68b27beb605c4c2afd092e150d
TommyAlv <tommyalvarado2@gmail.com>
2022-11-27 12:47:01 -0700

    graphql changes

--- server/server.js
+++ server/server.js
@@ -20,12 +20,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,'../client/build')))
-}
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+// Uncomment the bottom section when ready to deploy to heroku.
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname,'../client/build')))
+// }
+// app.get('*', (req,res)=>{
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'))
+// })
 
 const startApolloServer = async(typeDefs, resolvers) => {
 
